a5ed252a842c9ba2add124a0153f1539d9cfe8bd
pjh990909 <pjh990909@naver.com>
2024-03-20 16:23:41 +0900

    3/20

---
+++ vue-exam/src/views/Ex01View.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>vue공부</h1>
+    <h2>{{ subTitle }}</h2>
+    <p>{{ content }}</p>
+</template>
+
+<script>
+
+export default {
+    name: "Ex01View",
+    components: {},
+    data() {
+        return {
+            subTitle: "바인딩된 소제목",
+            content:"vue 열심히 하는중~"
+        };
+    },
+    methods: {},
+};
+</script>
+
+<style></style>
---
+++ vue-project/src/views/HelloView.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <h1>Hello Vue.js</h1>
+
+</template>
+
+<script>
+export default {
+    name: "HelloView",
+    data(){
+        return{
+
+        }
+
+    }
+}
+
+</script>
+
+<style></style>
